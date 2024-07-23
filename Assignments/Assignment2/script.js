207800d9fbe602106face565dc2dd729e354e0b0
Priyanshu Chaurasiya <sainty@Priyanshus-MacBook-Air.local>
2024-07-23 19:37:19 +0530

    Interactive counter with JS

---
+++ Assignments/Assignment2/script.js
@@ -0,0 +1,9 @@
+var num = 0;
+function increament() {
+    num++;
+    document.querySelector('.value').innerHTML = num;
+}
+function decreament() {
+    num--;
+    document.querySelector('.value').innerHTML = num;
+}
